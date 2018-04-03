4cf02963b349f980bb994c3abac8804c4e47b3f1
Thirumal S <thirumals@spritle.com>
2018-04-03 19:22:31 +0530

    Remove limit record

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user, foreign_key: "sender_id"
-  scope :for_display, -> { order(created_at: :desc).last(4) }
+  scope :for_display, -> { order(created_at: :desc) }
 
   def message_time
     self.created_at.in_time_zone(TZInfo::Timezone.get('Asia/Kolkata')).strftime("%A, %d %b %Y %l:%M %p")
